b449d7051374ae1e6d53faf6a020e4bf72c73026
User <macieksinicki@gmail.com>
2020-03-09 19:50:14 +0100

    Pollard-rho algorithm with saving values.

--- pollard_rho_logarithm.py
+++ pollard_rho_logarithm.py
@@ -1,15 +1,14 @@
-from random import randint
-
 _g = 3
-_y = 44
+_y = 48
 _p = 101
+
 
 class Sets:
     S0 = {}
     S1 = {}
     S2 = {}
 
-    def addToGroup(self, group_number, A, values) :
+    def addToGroup(self, group_number, A, values):
         if group_number == 0:
             self.S0[A] = values
         if group_number == 1:
@@ -23,9 +22,9 @@ def count_A(alfa, beta):
     return ((_g ** beta) * (_y ** alfa)) % _p
 
 
+# calculate value of X based on alfas and betas from collision
 def count_X(alfa1, beta1, alfa2, beta2):
-    print(alfa1, alfa2, beta1, beta2, (beta1 - beta2) % (_p - 1))
-    return ((beta1 - beta2) % (_p - 1)) / (alfa2 - alfa1)
+    return ((beta2 - beta1) % (_p - 1)) / (alfa1 - alfa2)
 
 
 def pollard_rho(sets):
@@ -34,36 +33,41 @@ def pollard_rho(sets):
     # beta- power of _g value
     beta = 10
 
+    A = count_A(alfa, beta)
     while True:
-        A = count_A(alfa, beta)
-        group_number = A % 3
-        print("alfa:  " + str(alfa) + "  beta:  " + str(beta) + "  value:  " + str(A) + "  group:  " + str(group_number))
-        old_values = (alfa, beta)
-        if group_number == 0:
+        set_number = A % 3
+        # if no collision detected values are added to sets as values with unique key
+        values_tosave = (alfa, beta)
+        A_tosave = A
+
+        if set_number == 0:
             if A in sets.S0.keys():
                 (_alfa, _beta) = sets.S0[A]
                 return count_X(alfa, beta, _alfa, _beta)
             else:
-                alfa *= 2
-                beta *= 2
+                alfa = alfa + 1
+                beta = beta
+                A = A * _y % _p
 
-        if group_number == 1:
+        if set_number == 1:
             if A in sets.S1.keys():
                 (_alfa, _beta) = sets.S1[A]
                 return count_X(alfa, beta, _alfa, _beta)
             else:
-                alfa = alfa + 1
-                beta = beta
+                alfa *= 2
+                beta *= 2
+                A = A ** 2 % _p
 
-        if group_number == 2:
+        if set_number == 2:
             if A in sets.S2.keys():
                 (_alfa, _beta) = sets.S2[A]
                 return count_X(alfa, beta, _alfa, _beta)
             else:
                 alfa = alfa
                 beta = beta + 1
+                A = A * _g % _p
 
-        sets.addToGroup(group_number, A, old_values)
+        sets.addToGroup(set_number, A_tosave, values_tosave)
 
 
 print(pollard_rho(Sets()))
